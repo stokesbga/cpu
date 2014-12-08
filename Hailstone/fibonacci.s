4442850c6f7e6c546aa949711284f661b3ac0e6d
Alexander Stokes <stokesa@wit.edu>
2014-12-07 19:43:20 -0500

    working hailstone and fibonacci

--- Hailstone/fibonacci.s
+++ Hailstone/fibonacci.s
@@ -5,8 +5,12 @@ main:	li r1, 3         		# n1
 		li r7, 1                # counter
 		
 loop:   add r0, r2, r1          # adds r2 and r1 in r0
-		move r2, r1
-		move r1, r0
-		addi r7, r7, 1			# THIS FUCKING WORKS I SWEAR JEESH
+		li r2, 0				# set to zero and mov r1 to r2
+		add r2, r1, r6
+		
+		li r1, 0				# set to zero and mov r0 to r1
+		add r1, r0, r6
+		
+		inc r7					# THIS FUCKING WORKS I SWEAR JEESH
 		jmp loop
         
--- Hailstone/hailstone.s
+++ Hailstone/hailstone.s
@@ -1,16 +1,26 @@
 # Authors: Alex Stokes and Matt Eisenberg
 
 main:	li r0, 41       		# n 
-		li r7, 0				# counter	
+		li r1, 0
+		li r2, 0
+		li r3, 0
+		li r4, 0
+		li r5, 0
+		li r7, 0				# counter
+		li r6, 1				# compare to me to terminate
 		
-loop:   remi r1, r0, 2
-        jnez r1, odd
+loop:   jeq r0, r6, done		# if 1, go to done
+		remi r1, r0, 2			# get remainder of r0/2
+        jnez r1, odd			# if not zero, its odd
 		
-even:   divi r0, r0, 2
-		addi r7, r7, 1
+even:   divi r0, r0, 2			# divide by 2
+		addi r7, r7, 1			# increment counter
 		jmp loop
 		
-odd:    muli r0, r0, 3
-        addi r0, r0, 1
-		addi r7, r7, 1
+odd:    muli r0, r0, 3			# 3x r0
+        inc r0					# add one
+		addi r7, r7, 1			# increment counter
 		jmp loop
+		
+		
+done:	jmp main
