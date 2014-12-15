ec1879d35dea2c569a6d4537d796e6109fdf352c
Matt Eisenberg <eisenbergm@wit.edu>
2014-12-14 22:56:35 -0500

    microcode tests
    all sortsa fun operations

---
+++ Hailstone/addTest.asm
@@ -0,0 +1,80 @@
+main:	#Addition test
+		li r0, 5
+		li r1, 4
+		add r2, r1, r0
+
+		li r0, 0
+		li r1, 0
+		li r2, 0
+		
+		#Subtraction test
+		li r0, 5
+		li r1, 4
+		sub r2, r0, r1
+		
+		li r0, 0
+		li r1, 0
+		li r2, 0
+		
+		#Multiplication test
+		li r0, 5
+		li r1, 4
+		mul r2, r0, r1
+
+		li r0, 0
+		li r1, 0
+		li r2, 0
+		
+		#Division test
+		li r0, 20
+		li r1, 4
+		div r2, r0, r1
+		
+		li r0, 0
+		li r1, 0
+		li r2, 0
+		
+		#Modulous test
+		li r0, 5
+		li r1, 4
+		rem r2, r0, r1
+	
+		li r0, 0
+		li r1, 0
+		li r2, 0
+		
+		#Logical shift LEFT test
+		li r0, 5
+		li r1, 4
+		lsl r2, r0, r1
+		
+		li r0, 0
+		li r1, 0
+		li r2, 0
+		
+		#Logical shift RIGHT test
+		li r0, 5
+		li r1, 4
+		lsr r2, r0, r1
+		
+		li r0, 0
+		li r1, 0
+		li r2, 0
+		
+		#Arithmetic shift RIGHT test
+		li r0, 5
+		li r1, 4
+		asr r2, r0, r1
+		
+		li r0, 0
+		li r1, 0
+		li r2, 0
+		
+		#Rotate LEFT test
+		li r0, 5
+		li r1, 4
+		rol r2, r0, r1
+		
+		li r0, 0
+		li r1, 0
+		li r2, 0
